abef382661156ad60dd4b63be70adfccf92e4b38
Erik Bjerned <erbj0497@tussilago.it.uu.se>
2023-04-16 21:09:50 +0200

    A2, setup

---
+++ A2/matrix.c
@@ -0,0 +1,84 @@
+#include <math.h>
+
+#include <mpi.h>
+#include <stdio.h>
+
+
+int main(int argc, char* argv[]){
+
+	if(argc != 3){
+		perror("USAGE: input_path, output_path");
+	}
+
+	int size;
+	int rank;
+ 
+
+	MPI_Init(&argc, &argv);
+	MPI_Datatype sub_matrix;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Status status;
+	
+	int side = sqrt((float)size);
+	int x_coord = rank%side;
+	int y_coord = rank == 0 ? 0 : rank/side;
+
+	printf("PE: %i, (%i, %i)\n", rank,x_coord, y_coord);
+
+
+
+	MPI_Finalize();
+
+}
+
+// TODO: Byt ut till Mattias variant
+
+int read_input(const char *file_name, double **values) {
+	FILE *file;
+	if (NULL == (file = fopen(file_name, "r"))) {
+		perror("Couldn't open input file");
+		return -1;
+	}
+	int num_values;
+	if (EOF == fscanf(file, "%d", &num_values)) {
+		perror("Couldn't read element count from input file");
+		return -1;
+	}
+	if (NULL == (*values = malloc(num_values * sizeof(double)))) {
+		perror("Couldn't allocate memory for input");
+		return -1;
+	}
+	for (int i=0; i<num_values; i++) {
+		if (EOF == fscanf(file, "%lf", &((*values)[i]))) {
+			perror("Couldn't read elements from input file");
+			return -1;
+		}
+	}
+	if (0 != fclose(file)){
+		perror("Warning: couldn't close input file");
+	}
+	return num_values;
+}
+
+
+int write_output(char *file_name, const double *output, int num_values) {
+	FILE *file;
+	if (NULL == (file = fopen(file_name, "w"))) {
+		perror("Couldn't open output file");
+		return -1;
+	}
+	for (int i = 0; i < num_values; i++) {
+		if (0 > fprintf(file, "%.4f ", output[i])) {
+			perror("Couldn't write to output file");
+		}
+	}
+	if (0 > fprintf(file, "\n")) {
+		perror("Couldn't write to output file");
+	}
+	if (0 != fclose(file)) {
+		perror("Warning: couldn't close output file");
+	}
+	return 0;
+}
+
