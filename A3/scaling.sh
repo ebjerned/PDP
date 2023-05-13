bb54f292be0523f918616075eaebe3dab1947be3
Erik Bjerned <ebjerned@rackham4.uppmax.uu.se>
2023-05-13 13:58:00 +0200

    A3

--- A3/scaling.sh
+++ A3/scaling.sh
@@ -13,9 +13,9 @@ export OMPI_MCA_btl_openib_allow_ib=1
 
 for k in 3
 do
-	for j in 2 32
+	for j in 32
 	do
-		for i in 125000000 250000000 500000000 1000000000
+		for i in 500000000 1000000000
 		do
 			echo $k $j $i
 			mpirun -np $j ./main /proj/uppmax2023-2-13/nobackup/qsort_indata/input$i.txt test.txt $k
