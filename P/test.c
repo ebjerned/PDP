2df9df718af2f86808c165f396c4a3299cc3a60d
Erik Bjerned <ebjerned@rackham3.uppmax.uu.se>
2023-05-21 18:50:42 +0200

    P

--- P/test.c
+++ P/test.c
@@ -109,8 +109,8 @@ int main(int argc, char* argv[]){
 
 		// Inner över
 		if(mycoords[0] == 0){
-			for(int i = 1; i < sideElementsPerProc-1; i++)
-				local_q[i] = 0;
+//			for(int i = 1; i < sideElementsPerProc-1; i++)
+				//local_q[i] = 0;
 		}else{
 			MPI_Recv(topDest, sideElementsPerProc, MPI_DOUBLE, down, 0, Cycle_Communication, &status);
 			for(int i = 1; i < sideElementsPerProc-1; i++)
