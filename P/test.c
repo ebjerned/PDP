048d7e943b693704da9e89623e3e4d1956f1d3a0
Erik Bjerned <ebjerned@rackham1.uppmax.uu.se>
2023-05-22 11:48:40 +0200

    P

--- P/test.c
+++ P/test.c
@@ -70,7 +70,7 @@ int main(int argc, char* argv[]){
 		double x_i = mycoords[1]*sideElementsPerProc+i%sideElementsPerProc;
 		double y_i = mycoords[0]*sideElementsPerProc+i/sideElementsPerProc;
 		//if(x_i != 0 && x_i != n-1 && y_i != 0 && y_i != n-1){	
-			local_d[i] =  y_i;//2*h*h*h*(x_i*(1-h*x_i)+y_i*(1-h*y_i));
+			local_d[i] =  2*h*h*h*(x_i*(1-h*x_i)+y_i*(1-h*y_i));
 		//} else {
 		//	local_d[i] = 0;
 		//}
